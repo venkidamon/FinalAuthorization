c8515ec5f08e275aeaffde2572cde658da59aec3
Venkatesh Kumar <venkateshkumartuty@gmail.com>
2022-05-17 17:22:57 +0530

    tempChange

--- Authorization/Controllers/WeatherForecastController.cs
+++ Authorization/Controllers/WeatherForecastController.cs
@@ -24,7 +24,7 @@ namespace Authorization.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = 20,
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
